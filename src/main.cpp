96319fce7ce76e8952be0270b92a23ff10df3fe0
hackrsackr <rob.knighton10@gmail.com>
2021-06-17 10:54:36 -0400

    update main to class

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,78 @@
+/*
+ * Class for Spunder
+ * A Spunder consists of a pressure transducer and a relay controlled solenoid valve
+ * 
+ * Default setup here is 4 Spunders one thermometer for all four (bulk-aging) Spunders
+ * Board - Arduino Mega
+ * ADC - 10 bit (0 - 1024)
+ * Transducer - 3 wires 5v, ground, signal
+ * Relay      - 2 wires switchleg and ground 
+*/
+
 #include <Arduino.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+using namespace std;
 
-#define NUMBER_OF_SPUNDERS 4           // number of sensor/relay pairs
-#define SENSOR_RATING 30               // rating of tranducers in psi
-#define OFFSET 102                     // bits from 0v - .5v
-#define FULLSCALE 922                  // bits from .5v - 4.5v
-#define RELAY_OPEN HIGH                // set relay open to HIGH
-#define ONE_WIRE_BUS 3                 // oneWire pin on arduino
+#define NUMBER_OF_SPUNDERS 4
+#define RELAY_OPEN HIGH
+#define ONE_WIRE_BUS 3
 
-// Setup a oneWire instance & pass it to Dallas Temperature
+// Setup a oneWire instance & pass it to Dallas Temperature.
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
-// define relay and sensor pins on the arduino
-const int RELAY_PINS[NUMBER_OF_SPUNDERS] = {7, 6, 5, 4};
+// Arduino pins and desired carbonation in vols.
 const int SENSOR_PINS[NUMBER_OF_SPUNDERS] = {0, 1, 2, 3};
-
-// desired carbonation in vols co2, array to store times of last vent
-float DESIRED_VOLS[NUMBER_OF_SPUNDERS] = {2.0, 2.5, 3.0, 3.5};
-int stored_times[NUMBER_OF_SPUNDERS];
-
-// define Spunder
-struct Spunder
+const int RELAY_PINS[NUMBER_OF_SPUNDERS] = {4, 5, 6, 7};
+float DESIRED_VOLS[NUMBER_OF_SPUNDERS] = {1.0, 2.0, 3.0, 4.0};
+class Spunder
 {
-  int id, RELAY_PIN, SENSOR_PIN, stored_time, vent;
-  float vols_setpoint, vols_value, psi_setpoint, psi_value, tempC, tempF;
-};
+public:
+  const static int sensor_rating = 60;     // Pressure rating of the transducer in PSI
+  const static int sensor_offset = 102;    // Bits from 0v - .5v
+  const static int sensor_fullscale = 922; // Bits from .5v - 5v
 
-// array of spunders
+  int id;          // Spunder ID number
+  int sensor_pin;  // Arduino pin of the transducer
+  int relay_pin;   // Arduino pin of the spunder valve relay
+  int stored_time; // Time of last vent
+  int vent;        // Time since last vent
+
+  float vols_setpoint; // Desired co2 in vols
+  float vols_value;    // Actual co2 in vols
+  float psi_setpoint;  // PSI target
+  float psi_value;     // Actual PSI
+  float tempC;         // Temp in Celsius
+  float tempF;         // Temp in Fahrenheit
+
+  void init_vols_setpoint(float vs) { vols_setpoint = vs; }
+
+  float get_tempC() { return sensors.getTempCByIndex(0); }
+
+  float get_tempF() { return sensors.getTempFByIndex(0); }
+
+  float get_psi_value() { return (analogRead(sensor_pin) - sensor_offset) * sensor_rating / float(sensor_fullscale - sensor_offset); }
+
+  float get_psi_setpoint(float vols_setpoint, float tempF) { return (-16.669 - (.0101059 * tempF)) + (.00116512 * (tempF * tempF)) + (.173354 * tempF * vols_setpoint) + (4.24267 * vols_setpoint) - (.0684226 * (vols_setpoint * vols_setpoint)); }
+
+  float get_vols_value(float psi_value, float psi_setpoint, float vols_setpoint) { return (psi_value / psi_setpoint) * vols_setpoint; }
+
+  int get_vent_value(float psi_value, float psi_setpoint, int stored_time, int relay_pin) { return (millis() - stored_time) / 60000; }
+};
 Spunder spunder_arr[NUMBER_OF_SPUNDERS];
 
-// setup
+// start serial and spunders, set valves to closed.
 void setup()
 {
-  // start serial and spunders, set valves to closed.
   Serial.begin(9600);
   sensors.begin();
-  // assign an id, relay, sensor, carb setpoint, start timer for each Spunder.
+
   for (int i = 0; i < NUMBER_OF_SPUNDERS; i++)
   {
     spunder_arr[i].id = i + 1;
-    spunder_arr[i].RELAY_PIN = RELAY_PINS[i];
-    spunder_arr[i].SENSOR_PIN = SENSOR_PINS[i];
+    spunder_arr[i].relay_pin = RELAY_PINS[i];
+    spunder_arr[i].sensor_pin = SENSOR_PINS[i];
     spunder_arr[i].vols_setpoint = DESIRED_VOLS[i];
     spunder_arr[i].stored_time = 0;
 
@@ -51,75 +81,31 @@ void setup()
   }
 }
 
-// get temperature in celsius
-float get_tempC()
-{
-  sensors.requestTemperatures();
-  float tempC = sensors.getTempCByIndex(0);
-  return tempC;
-}
-
-// get temperature in fahrenheit
-float get_tempF()
-{
-  sensors.requestTemperatures();
-  float tempF = sensors.getTempFByIndex(0);
-  return tempF;
-}
-
-// read raw data on SENSOR_PIN, return value in pounds per square inch
-float get_psi_value(int SENSOR_PIN)
-{
-  float psi_value = (analogRead(SENSOR_PIN) - OFFSET) * SENSOR_RATING / float((FULLSCALE - OFFSET));
-  return psi_value;
-}
-
-// derive psi setpoint for desired carbonation in vols co2
-float get_psi_setpoint(float vol_setpoint, float tempF)
-{
-  float psi_setpoint = (-16.669 - (.0101059 * tempF)) + (.00116512 * (tempF * tempF)) + (.173354 * tempF * vol_setpoint) + (4.24267 * vol_setpoint) - (.0684226 * (vol_setpoint * vol_setpoint));
-  return psi_setpoint;
-}
-
-// derive the current vols co2 based on the psi value and setpoint
-float get_vols_value(float psi_value, float psi_setpoint, float vols_setpoint)
-{
-  float vols_value = float(psi_value / psi_setpoint) * vols_setpoint;
-  return vols_value;
-}
-
-// get the time since last vent in minutes
-int get_vent_value(float psi_value, float psi_setpoint, int stored_time, int RELAY_PIN)
-{
-  int vent;
-  if (psi_value > psi_setpoint)
-  {
-    digitalWrite(RELAY_PIN, RELAY_OPEN);
-    delay(100);
-    digitalWrite(RELAY_PIN, !RELAY_OPEN);
-    stored_time = millis();
-    vent = 0;
-  }
-  else
-  {
-    vent = (millis() - stored_time) / 60000;
-  }
-  return vent;
-}
-
-// loop
 void loop()
 {
+  sensors.requestTemperatures();
   Serial.println("{");
   for (int i = 0; i < NUMBER_OF_SPUNDERS; i++)
   {
-    spunder_arr[i].tempC = get_tempC();
-    spunder_arr[i].tempF = get_tempF();
-    spunder_arr[i].psi_setpoint = get_psi_setpoint(spunder_arr[i].vols_setpoint, spunder_arr[i].tempF);
-    spunder_arr[i].psi_value = get_psi_value(SENSOR_PINS[i]);
-    spunder_arr[i].vols_value = get_vols_value(spunder_arr[i].psi_value, spunder_arr[i].psi_setpoint, spunder_arr[i].vols_setpoint);
-    spunder_arr[i].vent = get_vent_value(spunder_arr[i].psi_value, spunder_arr[i].psi_setpoint, spunder_arr[i].stored_time, spunder_arr[i].RELAY_PIN);
+    spunder_arr[i].tempC = spunder_arr[i].get_tempC();
+    spunder_arr[i].tempF = spunder_arr[i].get_tempF();
+    spunder_arr[i].psi_setpoint = spunder_arr[i].get_psi_setpoint(spunder_arr[i].vols_setpoint, spunder_arr[i].tempF);
+    spunder_arr[i].psi_value = spunder_arr[i].get_psi_value();
+    spunder_arr[i].vols_value = spunder_arr[i].get_vols_value(spunder_arr[i].psi_value, spunder_arr[i].psi_setpoint, spunder_arr[i].vols_setpoint);
+    spunder_arr[i].vent = spunder_arr[i].get_vent_value(spunder_arr[i].psi_value, spunder_arr[i].psi_setpoint, spunder_arr[i].stored_time, spunder_arr[i].relay_pin);
 
+    if (spunder_arr[i].psi_value > spunder_arr[i].psi_setpoint)
+    {
+      digitalWrite(spunder_arr[i].relay_pin, RELAY_OPEN);
+      delay(100);
+      digitalWrite(spunder_arr[i].relay_pin, !RELAY_OPEN);
+      spunder_arr[i].stored_time = millis();
+      spunder_arr[i].vent = 0;
+    }
+    else
+    {
+      spunder_arr[i].vent = (millis() - spunder_arr[i].stored_time) / 60000;
+    }
     Serial.print("\"");
     Serial.print(i + 1);
     Serial.print("\": ");
